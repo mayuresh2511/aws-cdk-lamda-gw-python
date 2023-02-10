15787561aa23cc719647131a481a03bf2873674e
Mayuresh Marathe <mayuresh.marathe123@gmail.com>
2023-02-10 23:16:16 +0530

    Initial AWS CDK Commit

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import aws_cdk as cdk
 
 from hello_cdk.hello_cdk_stack import HelloCdkStack
 
-
 app = cdk.App()
 HelloCdkStack(app, "hello-cdk")
 
--- hello_cdk/hello_cdk_stack.py
+++ hello_cdk/hello_cdk_stack.py
@@ -1,12 +1,10 @@
 from constructs import Construct
 from aws_cdk import (
-    Duration,
     Stack,
-    aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
+    aws_lambda as _lambda,
+    aws_apigateway as apigw
 )
+from CustomConstruct.hitcounter import HitCounter
 
 
 class HelloCdkStack(Stack):
@@ -14,13 +12,18 @@ class HelloCdkStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        queue = sqs.Queue(
-            self, "HelloCdkQueue",
-            visibility_timeout=Duration.seconds(300),
-        )
+        # my_lambda = _lambda.Function(
+        #     self, 'HelloNewHandler',
+        #     runtime=_lambda.Runtime.PYTHON_3_9,
+        #     code=_lambda.Code.from_asset('lambda'),
+        #     handler='hello.handler',
+        # )
+        #
+        # hello_with_hitcounter = HitCounter(
+        #     self, 'HelloHitCounter',
+        #     downstream=my_lambda
+        # )
+        #
+        # apigw.LambdaRestApi(self, "NewEndpoint",
+        #                     handler=my_lambda)
 
-        topic = sns.Topic(
-            self, "HelloCdkTopic"
-        )
-
-        topic.add_subscription(subs.SqsSubscription(queue))
